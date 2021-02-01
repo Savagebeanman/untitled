f1a3cd74582a80f7e37c5c6e299632c51f1b3bc7
Savagebeanman <72805052+Savagebeanman@users.noreply.github.com>
2021-01-31 21:04:13 -0500

    Update pxt.json, main.ts, main.py

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 while True:
-    if input.acceleration(Dimension.Z) > 0 or input.acceleration(Dimension.Z) < 0:
-        light.show_animation_frame(light.rainbowAnimation)
+    if input.acceleration(Dimension.Z) > 3 or input.acceleration(Dimension.Z) < -3:
+        light.set_all(light.rgb(0,255,0))
+        pause(3000)
         #music.wawawawaa.play_until_done()
     else:
         music.stop_all_sounds()
         light.clear()
+        pause(1000)
--- main.ts
+++ main.ts
@@ -1,10 +1,12 @@
 while (true) {
-    if (input.acceleration(Dimension.Z) > 0 || input.acceleration(Dimension.Z) < 0) {
-        light.showAnimationFrame(light.rainbowAnimation)
+    if (input.acceleration(Dimension.Z) > 3 || input.acceleration(Dimension.Z) < -3) {
+        light.setAll(light.rgb(0, 255, 0))
+        pause(3000)
     } else {
         // music.wawawawaa.play_until_done()
         music.stopAllSounds()
         light.clear()
+        pause(1000)
     }
     
 }
