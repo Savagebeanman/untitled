6f62aafd55388b397eb4d8e6c05d9dbb3ccd1ee8
Savagebeanman <72805052+Savagebeanman@users.noreply.github.com>
2021-01-31 20:17:39 -0500

    Update main.ts, main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 while True:
-    if input.acceleration(Dimension.Y) < 0 or input.acceleration(Dimension.Y) > 0 :
+    if input.acceleration(Dimension.Z) > 0 or input.acceleration(Dimension.Z) < 0:
         light.show_animation_frame(light.rainbowAnimation)
-        music.wawawawaa.play_until_done()
+        #music.wawawawaa.play_until_done()
     else:
         music.stop_all_sounds()
+        light.clear()
--- main.ts
+++ main.ts
@@ -1,9 +1,10 @@
 while (true) {
-    if (input.acceleration(Dimension.Y) < 0 || input.acceleration(Dimension.Y) > 0) {
+    if (input.acceleration(Dimension.Z) > 0 || input.acceleration(Dimension.Z) < 0) {
         light.showAnimationFrame(light.rainbowAnimation)
-        music.wawawawaa.playUntilDone()
     } else {
+        // music.wawawawaa.play_until_done()
         music.stopAllSounds()
+        light.clear()
     }
     
 }
