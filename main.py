d674c779a9c5e259d3a704f414d2fd544aa91953
Savagebeanman <72805052+Savagebeanman@users.noreply.github.com>
2021-02-02 10:50:51 -0500

    Update main.ts, main.py

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 while True:
+    music.set_volume(255)
+    light.set_brightness(255)
+    
     if input.acceleration(Dimension.Z) > 3 or input.acceleration(Dimension.Z) < -3:
         light.set_all(light.rgb(0,255,0))
         music.wawawawaa.play_until_done()
--- main.ts
+++ main.ts
@@ -1,4 +1,6 @@
 while (true) {
+    music.setVolume(255)
+    light.setBrightness(255)
     if (input.acceleration(Dimension.Z) > 3 || input.acceleration(Dimension.Z) < -3) {
         light.setAll(light.rgb(0, 255, 0))
         music.wawawawaa.playUntilDone()
