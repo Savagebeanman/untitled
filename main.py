cd07dde856aa2248f3b8b5eab0b05f086aa3c6f8
Savagebeanman <72805052+Savagebeanman@users.noreply.github.com>
2021-02-02 09:42:17 -0500

    Update main.ts, main.py

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 while True:
     if input.acceleration(Dimension.Z) > 3 or input.acceleration(Dimension.Z) < -3:
         light.set_all(light.rgb(0,255,0))
+        music.wawawawaa.play_until_done()
         pause(3000)
-        #music.wawawawaa.play_until_done()
+        
     else:
         music.stop_all_sounds()
-        light.clear()
+        light.set_all(light.rgb(255,0,0))
         pause(1000)
--- main.ts
+++ main.ts
@@ -1,11 +1,11 @@
 while (true) {
     if (input.acceleration(Dimension.Z) > 3 || input.acceleration(Dimension.Z) < -3) {
         light.setAll(light.rgb(0, 255, 0))
+        music.wawawawaa.playUntilDone()
         pause(3000)
     } else {
-        // music.wawawawaa.play_until_done()
         music.stopAllSounds()
-        light.clear()
+        light.setAll(light.rgb(255, 0, 0))
         pause(1000)
     }
     
